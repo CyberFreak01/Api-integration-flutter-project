d1383cad1228625d58c675a297bd7fc3a2b7234b
CyberFreak01 <sujalchauhan921@gmail.com>
2024-10-08 21:18:25 +0530

    update docs

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2859315624"
+    serviceWorkerVersion: "2811522808"
   }
 });
